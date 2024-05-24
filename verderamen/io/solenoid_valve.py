8287fe850683334a6bdf6f23bd3768ae7c5d3e28
HighSoftWare96 <bertonc96@gmail.com>
2024-05-24 12:33:05 +0200

    feat: conditional open valve

--- verderamen/io/solenoid_valve.py
+++ verderamen/io/solenoid_valve.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 class SolenoidValve:
   def __init__(self):
       self.valve_transistor_base_out_pin = get_config_key('VALVE_TRANSISTOR_BASE_OUT_PIN', int)
+      self.keep_valve_open = get_config_key('KEEP_VALVE_OPEN', str, False) == 'True'
 
   def setup(self):
      GPIO.setup(self.valve_transistor_base_out_pin, GPIO.OUT)
@@ -13,7 +14,10 @@ class SolenoidValve:
 
   def loop(self):
      # test
-     GPIO.output(self.valve_transistor_base_out_pin, GPIO.HIGH)
+     if(self.keep_valve_open):
+       GPIO.output(self.valve_transistor_base_out_pin, GPIO.HIGH)
+     else:
+      GPIO.output(self.valve_transistor_base_out_pin, GPIO.LOW)     
      is_open=GPIO.input(self.valve_transistor_base_out_pin)
      stats = {
         "is_open": is_open
