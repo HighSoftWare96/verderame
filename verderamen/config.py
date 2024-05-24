0c7624e66dc16ce9260c40b74617decc5a61a30b
HighSoftWare96 <bertonc96@gmail.com>
2024-05-24 16:27:46 +0200

    fix: condition

--- verderamen/config.py
+++ verderamen/config.py
@@ -3,6 +3,6 @@ from os import environ
 def get_config_key(key, ctor, defaultValue = None):
   if key in environ:
     return ctor(environ[key])
-  elif defaultValue is not None:
+  elif defaultValue is None:
     raise Exception(f'Configuration key {key} required!')
   return defaultValue
