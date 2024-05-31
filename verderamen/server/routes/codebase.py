1d72b48ec186b2ecfd580a7d8adfbfc40581b1d9
HighSoftWare96 <bertonc96@gmail.com>
2024-05-31 10:50:34 +0200

    fix: script

--- verderamen/server/routes/codebase.py
+++ verderamen/server/routes/codebase.py
@@ -5,7 +5,7 @@ import subprocess
 
 current_file_name=f"/{path.basename(__file__).replace('.py', '')}"
 current_folder=path.dirname(path.abspath(__file__))
-codebase_sync_script_path=Path(path.join(current_folder, '../../deploy/sync.sh')).resolve()
+codebase_sync_script_path=Path(path.join(current_folder, '../../../deploy/sync.sh')).resolve()
 
 def codebase_routes(app, prefix = current_file_name):
     @app.route(path.join(prefix, 'webhook'), methods=['POST'])
