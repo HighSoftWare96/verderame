b2d10df892721d16a5eab72737c0af67511ec80a
HighSoftWare96 <bertonc96@gmail.com>
2024-05-31 11:46:04 +0200

    fix: args

--- verderamen/server/main.py
+++ verderamen/server/main.py
@@ -4,4 +4,4 @@ from .server import build_server
 
 def main(to_core_q, to_server_q, event):
   logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-  build_server()
+  build_server(to_core_q, to_server_q)
