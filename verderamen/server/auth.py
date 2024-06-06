cf70a44fca8e46748af1fb821b816133cf810fc3
HighSoftWare96 <bertonc96@gmail.com>
2024-06-06 18:23:10 +0200

    fix: auth

--- verderamen/server/auth.py
+++ verderamen/server/auth.py
@@ -1,13 +1,20 @@
 from ..config import get_config_key
 from flask_httpauth import HTTPBasicAuth
 
-basic_auth = HTTPBasicAuth()
-admin_user = '$$admin'
-admin_password = get_config_key('TELEMETRIES_SERVER_PASSWORD', str)
+__basic_auth = None
+__admin_user = '$$admin'
+__admin_password = get_config_key('TELEMETRIES_SERVER_PASSWORD', str)
 
-@basic_auth.verify_password
-def verify_password(username, password):
-  print(username, password)
-  if username == admin_user or password == admin_password:
-      return username
-  return None
+def build_auth():
+  global __basic_auth
+  if __basic_auth is not None:
+     return __basic_auth
+  
+  __basic_auth = HTTPBasicAuth()
+  @__basic_auth.verify_password
+  def verify_password(username, password):
+    print(username, password)
+    if username == __admin_user or password == __admin_password:
+        return username
+    return None
+  return __basic_auth
--- verderamen/server/routes/telemetries.py
+++ verderamen/server/routes/telemetries.py
@@ -1,15 +1,17 @@
 from os import path
 from ...services.intercom import list_messages
-from ..auth import basic_auth
+from ..auth import build_auth
 from os import getpid
 
 current_file_name=f"/{path.basename(__file__).replace('.py', '')}"
 __cached_telemetries = None
 
 def telemetries_routes(to_core_q, to_server_q, app, prefix = current_file_name):
-    @app.route(path.join(prefix, ''), methods=['GET'])
-    @basic_auth.login_required
-    def getTelemetries():
+   auth = build_auth()
+
+   @app.route(path.join(prefix, ''), methods=['GET'])
+   @auth.login_required
+   def getTelemetries():
       msgs = list_messages(to_server_q)
       if bool(len(msgs)):
          __cached_telemetries = msgs[0].payload
